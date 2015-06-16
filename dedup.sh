5ba6fdfcedecb3c81b2fc15a48c5861bdb9cde9e
boblaublaw <github@sublimation.org>
2015-06-15 17:35:13 -0700

    added and ignore list and started "prescribe cmds"

--- dedup.sh
+++ dedup.sh
@@ -2,9 +2,11 @@
 
 SUFFIX=ddh
 HASH=sha1
+IGNORELIST=".DS_Store"
 
 # this will need to be tuned to the hash function of choice:
 function hashf() { 
+    /bin/echo -n "$HASH:"
     if [ x"$1" = x ]; then
         openssl $HASH | cut -f2 -d\= | cut -c2-
     else
@@ -13,7 +15,7 @@ function hashf() {
 }
 
 function dir_depth() {
-    dir=$1
+    dir="$1"
     maxdep=1
     last=-1
     while [ 1 ]; do
@@ -28,7 +30,7 @@ function dir_depth() {
 }
 
 function hash_dir() {
-    root=$1
+    root="$1"
 
     if [ ! -d "${root}" ]; then
         echo please supply a directory to hash
@@ -37,6 +39,17 @@ function hash_dir() {
 
     echo hashing all files in ${root}...
     find "$root" -type f  | grep -v ".${SUFFIX}$" | while read f; do 
+        basefile=`basename "$f"`
+        skip=0
+        for ignore in $IGNORELIST; do
+            if [ "$basefile" = "$ignore" ]; then   
+                skip=1
+                break
+            fi
+        done
+        if [ $skip -eq 1 ]; then
+            continue
+        fi
         hash="${f}.${SUFFIX}"
         if [ ! -f "${hash}" -o "${f}" -nt "${hash}" ]; then
             hashf "${f}" > "${hash}"
@@ -59,7 +72,7 @@ function hash_dir() {
 }
 
 function clean_hashes() {
-    root=$1
+    root="$1"
 
     if [ ! -d "${root}" ]; then
         echo please supply a directory to clean
@@ -69,7 +82,17 @@ function clean_hashes() {
     find "$root" -type f -name "*.${SUFFIX}" | while read f; do 
         rm "$f"
     done
-    rm ${root}.${SUFFIX}
+    rm -f ${root}.${SUFFIX}
+}
+
+function prescribe_cmds() {
+    root="$1"
+
+    find "$root" -name "*.${SUFFIX}" | while read hashfile; do 
+        file=`basename "$hashfile" .${SUFFIX}`
+        hash=`cat "$hashfile"`
+        echo $file\|$hash
+    done
 }
 
 function usage() {
@@ -81,6 +104,8 @@ dedup.sh usage:
 where <cmd> is one of:
     hd      (hash directories):     hash all the files and directories in a directory
     cd      (clean directories):    remove all the hashfiles in a directory
+    pc      (prescribe commands):   generate a list of commands to resolve dups
+
 EOF
 }
 
@@ -94,6 +119,11 @@ case $1 in
         shift
         echo cleaning $@
         clean_hashes $@
+        ;;
+    pc )
+        shift
+        echo prescribing commands for $@
+        prescribe_cmds $@
         ;;
     *)
         usage
