2fa1c6d6315783a0a2350551e43b77341d3519f0
boblaublaw <github@sublimation.org>
2015-06-25 15:54:53 -0700

    much better traversal and injest logic

--- dedup.py
+++ dedup.py
@@ -2,22 +2,37 @@
 
 import hashlib
 import os
+import sys
  
 # Set the directory you want to start from
 
-inputList = [ 'work_usb', 'test', 'dedup.py' ] 
 topLevelList = {}
 
 class dirObj():
-    def __init__(self, pathname):
-        self.pathname=pathname
+    def __init__(self, name, parent=None):
+        self.name=name
         self.files={}
         self.subdirs={}
+        self.parent=parent
+
+    def getLineage(self):
+        if self.parent == None:
+            return [ self.name ]
+        ancestry=self.parent.getLineage()
+        ancestry.append(self.name)
+        return ancestry
+    
+    def display(self):
+        print 'Directory ' + '/'.join(self.getLineage())
+        for name, entry in self.subdirs.iteritems():
+            entry.display()
+        for name, entry in self.files.iteritems():
+            entry.display();
 
     def placeDir(self, dirName):
-        print "looking to place " +  dirName + " in " + self.pathname
+        #print "looking to place " +  dirName + " in " + self.name
         dirList=dirName.split('/')
-        if dirList.pop(0) != self.pathname:
+        if dirList.pop(0) != self.name:
             raise LookupError
         
         if len(dirList) == 0:
@@ -25,22 +40,31 @@ class dirObj():
 
         nextDirName=dirList[0]
         if nextDirName in self.subdirs:
-            print "found " + nextDirName + " in " + self.pathname
+            #print "found " + nextDirName + " in " + self.name
             return self.subdirs[nextDirName].placeDir('/'.join(dirList))
 
-        print "did not find " + nextDirName + " in " + self.pathname
-        nextDir=dirObj(nextDirName)
+        #print "did not find " + nextDirName + " in " + self.name
+        nextDir=dirObj(nextDirName, self)
         self.subdirs[nextDirName]=nextDir
         return nextDir.placeDir('/'.join(dirList))
 
-    def placeFile(self, fileName):
-        self.files[fileName]=fileObj(fileName)
+    def placeFile(self, fileName, parent = None):
+        self.files[fileName]=fileObj(fileName, self)
     
 class fileObj():
-    def __init__(self, name):
+    def __init__(self, name, parent=None):
         self.name=name;
+        self.parent=parent
     
-for entry in inputList: 
+    def display(self):
+        if self.parent != None:
+            print "File " + '/'.join(self.parent.getLineage()) + '/' + self.name
+        else:
+            print "File " + self.name
+    
+sys.argv.pop(0)
+
+for entry in sys.argv:
     if os.path.isfile(entry):
         print 'Found a file:\t\t' + entry
         topLevelList[entry]=(fileObj(entry))
@@ -51,10 +75,13 @@ for entry in inputList:
             dirEntry=topDirEntry.placeDir(dirName)
             for fname in fileList:
                 print('\t\t\t%s/%s' % (dirName, fname))
-                #dirEntry.placeFile(fname)
+                dirEntry.placeFile(fname)
             print('Found directory:\t%s' % dirName)
     else:
         print "I don't know what this is" + entry
 
+print
+for name, entry in topLevelList.iteritems():
+    entry.display()
 
 
